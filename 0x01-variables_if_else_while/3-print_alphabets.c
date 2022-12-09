d3e0381ec7b8fe4aa739d69842e3425ea88a0410
wizsenay <tomasfrehiwot@gimal.com>
2022-12-09 07:55:54 -0800

    alpabetic c-programming

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char t;
+
+	char a;
+
+	t = 'a';
+	a = 'A';
+	while
+		(t <= 'z') {
+			putchar(t);
+			t++;
+		}
+	while
+		(a <= 'Z') {
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
