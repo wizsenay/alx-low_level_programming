477f974333c2018d66d7518b25b83ad59b30d25a
wizsenay <tomasfrehiwot@gimal.com>
2022-12-09 07:29:23 -0800

    alphabet_game alx project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char s;
+
+	s = 'a';
+	while
+		(s <= 'z') {
+			putchar(s);
+			s++;
+		}
+	putchar('\n');
+	return (0);
+
+}
