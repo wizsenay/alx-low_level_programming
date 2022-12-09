b308dcd1bd97690d9fe77d1bd0973eb490ba0c3c
wizsenay <tomasfrehiwot@gimal.com>
2022-12-09 08:59:05 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int y;
+
+	for (y = 0; y < 10; y++)
+		printf("%d", y);
+	putchar('\n');
+	return (0);
+}
