2fa89ce14c593e60b0f4aa9e72fef69495ef8e40
wizsenay <tomasfrehiwot@gimal.com>
2022-12-13 11:45:37 -0800

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ */
+
+void print_alphabet_x10(void);
+{
+	char alphabet;
+	int myNum = 0;
+
+	while (myNum < 10)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+			_putchar('\n');
+		}
+		_putchar('\n');
+		myNum++;
+	}
+}
