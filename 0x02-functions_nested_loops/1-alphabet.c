cc332fb3c53a5bf06a5218e58755c5b9f5c8e0ea
wizsenay <tomasfrehiwot@gimal.com>
2022-12-13 11:22:22 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+	_putchar('\n');
+}
