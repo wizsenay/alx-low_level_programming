c8b8233b8cdf5b0b376c8e63ace4b24368ab860a
wizsenay <tomasfrehiwot@gimal.com>
2022-12-14 00:36:23 -0800

    prints the 9 times table, starting with 0.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,12 +12,24 @@ void times_table(void)
 
 			c = a * b;
 
-			if (c <= 9)
+			if ((c / 10) == 0)
+			{
+				if (b == 0)
+
 				_putchar(',');
 				_putchar(' ');
+				
+				if (b != 0)
+					_putchar(' ');
+				_putchar(c + '0');
+			}
 			else
 				_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
+				_putchar((c % 10) + '0');
+				if (b == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
 		}
 		_putchar('\n');
 	}
