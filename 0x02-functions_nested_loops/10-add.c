e222c09f6a120977acf27ee7a1eab1e61ff3baff
wizsenay <tomasfrehiwot@gimal.com>
2022-12-14 01:07:53 -0800

    that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * add - Check Holberton
+ * @a: An integer a
+ * @b: An integer b
+ * Description: Function that adds two integers and retus the result
+ * Return: Result to add a and b
+ */
+
+int add(int a, int b)
+
+{
+
+	return (a + b);
+
+}
