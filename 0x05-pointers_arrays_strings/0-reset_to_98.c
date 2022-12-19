53e55a9e3403aa22010931140975f771728cf175
wizsenay <tomasfrehiwot@gimal.com>
2022-12-19 09:05:36 -0800

    pointer to an int as parameter and updates the value it points to to 98.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,24 +2,9 @@
 /**
  * reset_to_98 - function that takes a pointer
  * @n: input pointer
- *Return: 0
  */
 
 void reset_to_98(int *n)
 {
-	int a;
-	int *prt;
-
-	a = 420
-	prt = &a
-
-	_putchar("%d", n);
-	_putchar("\n");
-
-	*prt = 98;
-
-	_putchar("%d", n);
-	_putchar("\n");
-
-	return (0);
+	*n = 98;
 }
