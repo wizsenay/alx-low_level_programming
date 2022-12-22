48277297aa88bdf42d071cf83df7f45c0451b2d0
wizsenay <tomasfrehiwot@gimal.com>
2022-12-22 09:29:07 -0800

    a function that changes all lowercase letters of a string to uppercase.

---
+++ 0x06-pointers_arrays_strings/5-senay.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+
+   * main - check the code for Holberton School students.
+
+    *
+
+     * Return: Always 0.
+
+      */
+
+int main(void)
+
+{
+
+	  char s[] = "Holberton School!\n";
+
+	    char *p;
+
+
+
+	      p = string_toupper(s);
+
+	        printf("%s", p);
+
+		  printf("%s", s);
+
+		    return (0);
+
+}
