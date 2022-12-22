141da234f718804f651cbe02dba95cc1fd782a5d
wizsenay <tomasfrehiwot@gimal.com>
2022-12-22 08:21:25 -0800

    a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 		dest++;
 
 	while (*src)
-		*dest++ = *scr++;
+		*dest++ = *src++;
 
 	*dest = '\0';
 
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 
 
-#include <stdio>
+#include <stdio.h>
 
 
 int _putchar(char c);
