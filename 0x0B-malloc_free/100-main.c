e8d1e6bba40ee20913fe34376ea0a7d67df3888c
wizsenay <tomasfrehiwot@gimal.com>
2023-01-11 07:30:49 -0800

    a function that concatenates all the arguments of your program.

---
+++ 0x0B-malloc_free/100-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+int main(int ac, char *av[])
+
+{
+
+	    char *s;
+
+
+
+	        s = argstostr(ac, av);
+
+		    if (s == NULL)
+
+			        {
+
+					        return (1);
+
+						    }
+
+		        printf("%s", s);
+
+			    free(s);
+
+			        return (0);
+
+}
