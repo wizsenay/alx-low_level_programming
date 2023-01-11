390427d490e7d7f73a8c2dcb7b3a7b309f4a42c2
wizsenay <tomasfrehiwot@gimal.com>
2023-01-11 06:59:57 -0800

    a function that concatenates two strings.

---
+++ 0x0B-malloc_free/2-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+int main(void)
+
+{
+
+	    char *s;
+
+
+
+	        s = str_concat("Betty ", "Holberton");
+
+		    if (s == NULL)
+
+			        {
+
+					        printf("failed\n");
+
+						        return (1);
+
+							    }
+
+		        printf("%s\n", s);
+
+			    free(s);
+
+			        return (0);
+
+}
