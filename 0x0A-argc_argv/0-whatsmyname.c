3d242d7c5054b1f8d566bf4778669edd57a725ac
wizsenay <tomasfrehiwot@gmail.com>
2023-10-28 02:22:22 -0700

    a program that prints its name, followed by a new line. only it takes the name from the comand line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * main - prints its name
+ * @argc: count the string
+ * @argv: input strings form the comand line.
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	int i;
