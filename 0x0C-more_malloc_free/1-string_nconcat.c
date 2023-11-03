ffe8181c062bddc52e68d19aa9eae8a793ed8dd9
wizsenay <tomasfrehiwot@gmail.com>
2023-11-02 23:51:06 -0700

    update the  function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * string_nconcat - concatenates two strings.
  * @s1:the first input string
@@ -11,37 +10,41 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *conc;
-	unsigned int i, j;
+	unsigned int i, j, k = 0, l = 0;
 
-	j = strlen(s1) + strlen(s2);
 	if (s1 == NULL)
-		s1 = "\0";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = "\0";
-	if (n < strlen(s2))
+		s2 = "";
+	while (s1[k])
+		k++;
+	while (s2[l])
+		l++;
+	j = k + l;
+	if (n < k)
 	{
-		conc = malloc(sizeof(char) * (strlen(s1) + n) + 1);
+		conc = malloc(sizeof(char) * (k + n) + 1);
 		if (conc == NULL)
 			exit(0);
-		for(i = 0; i < (strlen(s1) + n); i++)
+		for (i = 0; i < (k + n); i++)
 		{
-			if (i < strlen(s1))
+			if (i < k)
 				conc[i] = s1[i];
 			else
-				conc[i] = s2[i - strlen(s1)];
+				conc[i] = s2[i - k];
 		}
 	}
-	if (n >= strlen(s2))
+	if (n >= k)
 	{
 		conc = malloc(sizeof(char) * j + 1);
 		if (conc == NULL)
 			exit(0);
-		for(i = 0; i < j; i++)
+		for (i = 0; i < j; i++)
 		{
-			if (i < strlen(s1))
+			if (i < k)
 				conc[i] = s1[i];
 			else
-				conc[i] = s2[i - strlen(s1)];
+				conc[i] = s2[i - k];
 		}
 	}
 	return (conc);
