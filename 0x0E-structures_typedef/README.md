d3db41f9081869cdca17bc155e0076fdb7a23db9
wizsenay <tomasfrehiwot@gmail.com>
2023-11-06 08:23:09 -0800

    the first stract

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+#C - Structures, typedef
+
+some taskes relate with C - Structures and typedef
+these taskes given by alx softwer engineering class
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - contian char*, float
+ * @name: name dog
+ * @age: age of dog
+ * @owner: the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
