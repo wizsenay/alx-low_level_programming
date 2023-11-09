82a8f2b5d37b867ff94238ef7aa7ac7fc9102136
wizsenay <tomasfrehiwot@gmail.com>
2023-11-09 04:21:38 -0800

    update file 2-print_strings.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,13 +16,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ptr, int));
-		if (i == (n - 1))
-			printf("\n");
-		else
-		{
-			if (separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 				printf("%s ", separator);
 		}
 	}
+	printf("\n");
 	va_end(ptr);
 }
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+/**
+ * print_all - prints anything.
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ptr;
+	size_t i;
+	char *c;
+
+	i = 0;
+	va_start(ptr, format);
+	while (i < strlen(format))
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s", va_arg(ptr, char *));
+				break;
+			case 'i':
+				printf("%i", va_arg(ptr, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ptr, double));
+				break;
+			case 's':
+				{
+				c = va_arg(ptr, char *);
+				if (c != NULL)
+					printf("%s", c);
+				if (c == NULL)
+					printf("(nil)");
+				}
+				break;
+		}
+		if (i == (strlen(format) - 1))
+			printf("\n");
+		if (i != (strlen(format) - 1))
+			printf(" ");
+	}
+	va_end(ptr);
+}
