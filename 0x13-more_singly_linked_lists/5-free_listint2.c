b91e89a2a115d9705be2a2e410113c5c1c0acf3b
wizsenay <tomasfrehiwot@gmail.com>
2023-11-20 01:00:08 -0800

    add some file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - frees a list_t list.
+ * @head: the head pointer to nodes
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+		return;
+	while ((*head)->next != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	free(*head);
+	*head = NULL;
+}
