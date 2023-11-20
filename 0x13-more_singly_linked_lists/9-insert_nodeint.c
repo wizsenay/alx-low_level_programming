f3520da5ba05fc2465fea844860d5dc4e6bf0492
wizsenay <tomasfrehiwot@gmail.com>
2023-11-20 08:43:50 -0800

    add a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,34 +2,32 @@
 #include <stdlib.h>
 /**
  * insert_nodeint_at_index - inserts a new node at a given position.
- * @head: a pointer to the head pointer
- * @idx: the index of the list where the new node should be added.
- * Retrun: the address of the new node, or NULL if it failed
+ * @head: pointer to head pointer of the node
+ * @idx:  the index of the list where the new node should be added.
+ * @n: the data of a node
+ * Return: the address of the new node, or NULL if it failed
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newNode;
-	unsigned int i = 0;
+	unsigned int i;
+	listint_t *newNode, *temp;
 
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = n;
 	newNode->next = NULL;
-	if (*head == NULL)
-	{
-		*head = newNode;
-		return (*head);
-	}
-	if (head == NULL)
+
+	if (*head == NULL || head == NULL)
 		return (newNode);
-	while (i < idx)
+	temp = *head;
+	for (i = 0; i <= (idx + 1); i++)
 	{
-		(*head)->next = ((*head)->next)->next;
+		temp = temp->next;
 		i++;
 	}
-	newNode = ((*head)->next)->next;
-	(*head)->next= newNode;
-	return (*head);
+	newNode->next = temp->next;
+	temp->next = newNode;
+	return	(*head);
 }
